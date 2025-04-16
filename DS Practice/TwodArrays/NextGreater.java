c3de1fff3b034e990d50a04d14cd45a2797d98be
Vaibhav Mishra <156510179+mishra0303@users.noreply.github.com>
2025-04-16 22:05:23 +0530

    Delete DS Practice/TwodArrays directory

--- DS Practice/TwodArrays/NextGreater.java
+++
@@ -1,9 +0,0 @@
-public class NextGreater {
-    public static int nextGreater(int arr[]){
-
-    }
-    public static void main(String[] args) {
-        int arr[]={2,8,4,2,1};
-        
-    }
-}
--- DS Practice/TwodArrays/Subset.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class Subset {
-
-    public static List<List<Integer>> generateSubset(int arr[]){
-
-    }
-    
-}
